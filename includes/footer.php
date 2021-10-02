bd6bc3802bfb90000fdf6d85d71cc18cf13829ac
Aaron Cartledge <s3840848@student.rmit.edu.au>
2021-10-02 23:56:44 +1000

    added footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,17 @@
 <nav class="navbar navbar-expand-sm bg-dark text-white fixed-bottom py-2">
     <ul class="navbar-nav mr-auto mx-5 px-5">
         <li class="nav-item">
-            © &lt;Website Name Here&gt;
+            Secure E-Commerce 2021
+        </li>
+    </ul>
+    <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+            Practical Assignment
+        </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item mx-5 px-5">
+            INTE1070
         </li>
     </ul>
 </nav>
--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-sm bg-info navbar-dark">
     <ul class="navbar-nav big-font mr-auto mx-5 pl-5">
         <li class="nav-item pr-5">
-            <a class="nav-link text-light bg-info" href="index.php">SEC 2021</a>
+            <a class="nav-link text-light bg-info">Payment Gateway</a>
         </li>
         <li class="nav-item">
             <a class="nav-link text-light bg-info" href="index.php">Home</a>
