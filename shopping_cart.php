da38d3b5e193239d913cd3f420f9ccb45ef7c512
Aaron Cartledge <s3840848@student.rmit.edu.au>
2021-10-05 23:14:45 +1100

    updated navbar and styling for shopping cart

--- shopping_cart.php
+++ shopping_cart.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-     <!-- Required meta tags -->
-     <meta charset="utf-8">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="styling/style.css">
@@ -32,139 +32,136 @@
         }
 
         th {
-            background-color: #4CAF50;
+            background-color: #11ABA6;
             color: white;
         }
     </style>
+    <title>Shopping Cart</title>
 </head>
 
 <body>
-   
-<?php
+
+    <?php
     require_once("includes/header.php");
-?>
-    <h1>Shopping Cart</h1>
-    <form action="order_details.php" method="post">
-        <table>
-            <tr>
-                <th>Products</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Subtotal</th>
-            </tr>
-            <tr>
-                <td>Apples<input type="hidden" name="ProductA" id="ProductA" value="Apples" /></td>
-                <td>$10<input type="hidden" name="ProductAprice" id="ProductAprice" value="10" /></td>
-                <td><input id="ProductAquantity" name="ProductAquantity" type="number" value="0" min="0" max="10" /></td>
-                <td><input id="RemoveProductA" type="button" onclick="removeProduct();" value="Remove"></td>                <td>
-                    <p id="ProductAsubtotal">0</p><input id="ProductAtotal" name="ProductAtotal" type="hidden" />
-                </td>
-            </tr>
-            <tr>
-                <td>Oranges<input type="hidden" name="ProductB" id="ProductB" value="Oranges" /></td>
-                <td>$15<input type="hidden" name="ProductBprice" id="ProductBprice" value="15" /></td>
-                <td><input id="ProductBquantity" name="ProductBquantity" type="number" value="0" min="0" max="10" /></td>
-                <td><input id="RemoveProductB" type="button" onclick="removeProduct();" value="Remove"></td>                <td>
-                    <p id="ProductBsubtotal">0</p><input id="ProductBtotal" name="ProductBtotal" type="hidden" />
-                </td>
-            </tr>
-            <tr>
-                <td>Bananas<input type="hidden" name="ProductC" id="ProductC" value="Bananas" /></td>
-                <td>$20<input type="hidden" name="ProductCprice" id="ProductCprice" value="20" /></td>
-                <td><input id="ProductCquantity" name="ProductCquantity" type="number" value="0" min="0" max="10" /></td>
-                <td><input id="RemoveProductC" type="button" onclick="removeProduct();" value="Remove"></td>
-                <td>
-                    <p id="ProductCsubtotal">0</p><input id="ProductCtotal" name="ProductCtotal" type="hidden" />
-                </td>
-            </tr>
-            <tr>
-                <th></th>
-                <th>Total</th>
-                <th>
-                    <p id="Quantity">0</p><input id="totalQuantity" name="totalQuantity" type="hidden" />
-                </th>
-                <th>
-                    <p id="Price">0</p><input id="totalPrice" name="totalPrice" type="hidden" />
-                </th>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td><input id="ccNumber" type="text" placeholder="Enter your credit card no." name="ccNumber" required></button></td>
-                <td><input id="UpdateBtn" type="button" onclick="updateCart();" value="Update"></button></td>
-                <td><button type="submit">Submit</button></td>
-            </tr>
-        </table><br /><br />
-    </form>
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        header('Location: login_form.php');
+    }
+    ?>
+    <div class="container mt-3">
+        <h1 class="mb-3">Shopping Cart</h1>
+        <form action="order_details.php" method="post">
+            <table>
+                <tr>
+                    <th>Products</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Subtotal</th>
+                    <th>Remove Product</th>
+                </tr>
+                <tr>
+                    <td>Apples<input type="hidden" name="ProductA" id="ProductA" value="Apples" /></td>
+                    <td>$10<input type="hidden" name="ProductAprice" id="ProductAprice" value="10" /></td>
+                    <td><input id="ProductAquantity" name="ProductAquantity" type="number" value="0" min="0" max="10" /></td>
+                    <td>
+                        <p id="ProductAsubtotal">0</p><input id="ProductAtotal" name="ProductAtotal" type="hidden" />
+                    </td>
+                    <td><input id="RemoveProductA" class="btn btn-danger" type="button" onclick="removeProduct();" value="Remove"></td>
+                </tr>
+                <tr>
+                    <td>Oranges<input type="hidden" name="ProductB" id="ProductB" value="Oranges" /></td>
+                    <td>$15<input type="hidden" name="ProductBprice" id="ProductBprice" value="15" /></td>
+                    <td><input id="ProductBquantity" name="ProductBquantity" type="number" value="0" min="0" max="10" /></td>
+                    <td>
+                        <p id="ProductBsubtotal">0</p><input id="ProductBtotal" name="ProductBtotal" type="hidden" />
+                    </td>
+                    <td><input id="RemoveProductB" class="btn btn-danger" type="button" onclick="removeProduct();" value="Remove"></td>
+                </tr>
+                <tr>
+                    <td>Bananas<input type="hidden" name="ProductC" id="ProductC" value="Bananas" /></td>
+                    <td>$20<input type="hidden" name="ProductCprice" id="ProductCprice" value="20" /></td>
+                    <td><input id="ProductCquantity" name="ProductCquantity" type="number" value="0" min="0" max="10" /></td>
+                    <td>
+                        <p id="ProductCsubtotal">0</p><input id="ProductCtotal" name="ProductCtotal" type="hidden" />
+                    </td>
+                    <td><input id="RemoveProductC" class="btn btn-danger" type="button" onclick="removeProduct();" value="Remove"></td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <th>Total</th>
+                    <th>
+                        <p id="Quantity">0</p><input id="totalQuantity" name="totalQuantity" type="hidden" />
+                    </th>
+                    <th>
+                        <p id="Price">0</p><input id="totalPrice" name="totalPrice" type="hidden" />
+                    </th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input id="ccNumber" type="text" placeholder="Enter your credit card no." name="ccNumber" required></button></td>
+                    <td><input id="UpdateBtn" class="btn btn-success" type="button" onclick="updateCart();" value="Update"></button></td>
+                    <td><button type="submit" class="btn btn-primary">Submit</button></td>
+                </tr>
+            </table><br /><br />
+        </form>
+    </div>
     <script type="text/javascript">
         function calcSubTotal(productName) {
 
-                var quantity;
-                var price;
-                var subtotal;
-                if (document.getElementById(productName) == null) {
-                    quantity = 0;
-                    price = 0;
-                    subtotal = 0;
-                    return 0;  
+            var quantity;
+            var price;
+            var subtotal;
+            if (document.getElementById(productName) == null) {
+                quantity = 0;
+                price = 0;
+                subtotal = 0;
+                return 0;
+            } else {
+                quantity = parseInt(document.getElementById(productName + 'quantity').value);
+                if (quantity > 0) {
+                    price = parseInt(document.getElementById(productName + 'price').value);
+                    subtotal = price * quantity;
+                    document.getElementById(productName + "subtotal").innerHTML = subtotal;
+                    document.getElementById(productName + "total").value = subtotal;
+                    return subtotal;
                 }
-                
-                else {
-                    quantity = parseInt(document.getElementById(productName + 'quantity').value);
-            if (quantity > 0) {
-                price = parseInt(document.getElementById(productName + 'price').value);
-                subtotal = price * quantity;
-                document.getElementById(productName + "subtotal").innerHTML = subtotal;
-                document.getElementById(productName + "total").value = subtotal;
-                return subtotal;
-            }
-            document.getElementById(productName + "subtotal").innerHTML = 0;
-            document.getElementById(productName + "total").value = 0;
-            return 0;
+                document.getElementById(productName + "subtotal").innerHTML = 0;
+                document.getElementById(productName + "total").value = 0;
+                return 0;
 
-        }  
-            
-            
+            }
+
+
         }
 
         function updateCart() {
 
             var total = calcSubTotal('ProductA') + calcSubTotal('ProductB') + calcSubTotal('ProductC');
 
-           
+
 
             if (document.getElementById('ProductAquantity') == null && document.getElementById('ProductBquantity') == null && document.getElementById('ProductCquantity') == null) {
                 var quantity = 0;
-            }
-
-            else if (document.getElementById('ProductAquantity') == null && document.getElementById('ProductBquantity') == null) {
+            } else if (document.getElementById('ProductAquantity') == null && document.getElementById('ProductBquantity') == null) {
                 var quantity = parseInt(document.getElementById('ProductCquantity').value);
-            }
-
-            else if (document.getElementById('ProductAquantity') == null && document.getElementById('ProductCquantity') == null) {
+            } else if (document.getElementById('ProductAquantity') == null && document.getElementById('ProductCquantity') == null) {
                 var quantity = parseInt(document.getElementById('ProductBquantity').value);
-            }
-
-            else if (document.getElementById('ProductBquantity') == null && document.getElementById('ProductCquantity') == null) {
+            } else if (document.getElementById('ProductBquantity') == null && document.getElementById('ProductCquantity') == null) {
                 var quantity = parseInt(document.getElementById('ProductAquantity').value);
-            }
-
-            else if (document.getElementById('ProductAquantity') == null) {
+            } else if (document.getElementById('ProductAquantity') == null) {
                 var quantity = parseInt(document.getElementById('ProductBquantity').value) + parseInt(document.getElementById('ProductCquantity').value);
-            }
-
-            else if (document.getElementById('ProductBquantity') == null) {
+            } else if (document.getElementById('ProductBquantity') == null) {
                 var quantity = parseInt(document.getElementById('ProductAquantity').value) + parseInt(document.getElementById('ProductCquantity').value);
 
-            }
-            else if (document.getElementById('ProductCquantity') == null) {
+            } else if (document.getElementById('ProductCquantity') == null) {
                 var quantity = parseInt(document.getElementById('ProductAquantity').value) + parseInt(document.getElementById('ProductBquantity').value);
-            }
-            else {
+            } else {
                 var quantity = parseInt(document.getElementById('ProductAquantity').value) + parseInt(document.getElementById('ProductBquantity').value) + parseInt(document.getElementById('ProductCquantity').value);
             }
 
-           
+
             document.getElementById("Quantity").innerHTML = quantity;
             document.getElementById("totalQuantity").value = quantity;
             document.getElementById("Price").innerHTML = total;
@@ -174,51 +171,50 @@
         function removeProduct(productName) {
             $(this).closest("tr").remove();
             updateCart();
-           
+
         }
 
         function pageInit() {
-        var btn = document.getElementById('RemoveProductA');
-        var btn2 = document.getElementById('RemoveProductB');
-        var btn3 = document.getElementById('RemoveProductC');
-        var updatebtn = document.getElementById('UpdateBtn');
-      
-        // DOM2 standard
-        btn.addEventListener('click', removeProduct, false);
-        btn2.addEventListener('click', removeProduct, false);
-        btn3.addEventListener('click', removeProduct, false);
-        updatebtn.addEventListener('click', updateCart, false);
+            var btn = document.getElementById('RemoveProductA');
+            var btn2 = document.getElementById('RemoveProductB');
+            var btn3 = document.getElementById('RemoveProductC');
+            var updatebtn = document.getElementById('UpdateBtn');
+
+            // DOM2 standard
+            btn.addEventListener('click', removeProduct, false);
+            btn2.addEventListener('click', removeProduct, false);
+            btn3.addEventListener('click', removeProduct, false);
+            updatebtn.addEventListener('click', updateCart, false);
 
         }
-
     </script>
 
 
-<script src="rsa.js"></script>
+    <script src="rsa.js"></script>
     <script type="text/javascript">
-		function RSA_encryption(element){
+        function RSA_encryption(element) {
 
-			var plaintext = document.getElementById(element).value;
-			var public_key = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzdxaei6bt/xIAhYsdFdW62CGTpRX+GXoZkzqvbf5oOxw4wKENjFX7LsqZXxdFfoRxEwH90zZHLHgsNFzXe3JqiRabIDcNZmKS2F0A7+Mwrx6K2fZ5b7E2fSLFbC7FsvL22mN0KNAp35tdADpl4lKqNFuF7NT22ZBp/X3ncod8cDvMb9tl0hiQ1hJv0H8My/31w+F+Cdat/9Ja5d1ztOOYIx1mZ2FD2m2M33/BgGY/BusUKqSk9W91Eh99+tHS5oTvE8CI8g7pvhQteqmVgBbJOa73eQhZfOQJ0aWQ5m2i0NUPcmwvGDzURXTKW+72UKDz671bE7YAch2H+U7UQeawwIDAQAB-----END PUBLIC KEY-----";
-			// Encrypt with the public key...
-			var encrypt = new JSEncrypt();
-			encrypt.setPublicKey(public_key);
-			var encrypted = encrypt.encrypt(plaintext);
-		  
-			document.getElementById(element).value = encrypted;	
-		}
-	</script>
+            var plaintext = document.getElementById(element).value;
+            var public_key = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzdxaei6bt/xIAhYsdFdW62CGTpRX+GXoZkzqvbf5oOxw4wKENjFX7LsqZXxdFfoRxEwH90zZHLHgsNFzXe3JqiRabIDcNZmKS2F0A7+Mwrx6K2fZ5b7E2fSLFbC7FsvL22mN0KNAp35tdADpl4lKqNFuF7NT22ZBp/X3ncod8cDvMb9tl0hiQ1hJv0H8My/31w+F+Cdat/9Ja5d1ztOOYIx1mZ2FD2m2M33/BgGY/BusUKqSk9W91Eh99+tHS5oTvE8CI8g7pvhQteqmVgBbJOa73eQhZfOQJ0aWQ5m2i0NUPcmwvGDzURXTKW+72UKDz671bE7YAch2H+U7UQeawwIDAQAB-----END PUBLIC KEY-----";
+            // Encrypt with the public key...
+            var encrypt = new JSEncrypt();
+            encrypt.setPublicKey(public_key);
+            var encrypted = encrypt.encrypt(plaintext);
 
-  
-<script type="text/javascript">
-pageInit();
-</script>
+            document.getElementById(element).value = encrypted;
+        }
+    </script>
 
-        
-<?php
+
+    <script type="text/javascript">
+        pageInit();
+    </script>
+
+
+    <?php
     require_once("includes/footer.php");
-?>
-  
+    ?>
+
 </body>
 
 </html>
