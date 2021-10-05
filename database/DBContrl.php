60b1e667f7c66676dabfc8ab0ff6f7dc69fc4755
Patrick <s3508768@student.rmit.edu.au>
2021-10-05 12:21:33 +1100

    DBContrl Fixed

--- database/DBContrl.php
+++ database/DBContrl.php
@@ -6,21 +6,55 @@ private $host = "localhost";
 private $user = "root";
 private $password = "pass";
 private $database = "shoppingcart";
-$connection;
 
+public $connection;
+public $tbname;
 
 function intialise() {
     $this->connection = $this->connectionDB();
     }
 //Creates Database Connection
 function connectionDB() {
-    $connection = mysqli_connect($this->host, $this->user, $this->password, $this->database);
-    return $connection;
+    $connection = mysqli_connect($this->host, $this->user, $this->password);
+//Check Connection - If connection fails
+if ($connection) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+//Run Query - Will create database if it doesnt exist.
+$db = "CREATE DATABASE IF NOT EXISTS $database";
+    if (mysqli_query($connection, $db)) {
+        $connection = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+    
+//Create New Table in SQL
+//Creates table if it doesnt exist. Product name will have character limit of 50
+//Price is in Float for decimals
+//product_image 150 incase links are super long in character.
+$db = "CREATE TABLE IF NOT EXISTS $tbname
+        (id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        product_name VARCHAR (30) NOT NULL,
+        product_price FLOAT,
+        product_image VARCHAR(150)
+        );";
     }
 
-//Check Connection - If connection fails
-if ($connection->connect_error) {
-    die("Connection Failed: " . $connection->connect_error);
+//Check 
+if (!mysqli_query($connection, $db)) {
+    echo "Error while creating table: " . mysqli_error($connection);
+}
+//else
+else {  
+    return false;
+}
+
+//Function to select all data from the database
+function retrieveData() {
+    $db = "SELECT * FROM $this->tbname";
+    $results = mysqli_query($connection,$db);
+    if (mysqli_num_rows($results) > 0 ) {
+        return $results;
+    }
+}
+
 }
 }
-?>
